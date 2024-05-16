39a8d17512604b2480d394bbf184077ea8319bfd
DermisZabala <dermiszabala@gmail.com>
2024-05-16 17:43:16 -0400

    POO Clase 6

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace POO6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            realizarTareas();
+        }
+
+        static void realizarTareas()
+        {
+            Punto origen = new Punto(4,10);
+            Punto destino = new Punto();
+        }
+
+    }
+}
---
+++ Punto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO6
+{
+    internal class Punto
+    {
+        int x, y;
+        public Punto(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+            Console.WriteLine($"El valor de X es {x}, el valor de y es {y}");
+        }
+        public Punto()
+        {
+            Console.WriteLine("Este es el constructor sin parámetros");
+        }
+    }
+}
